5e43f6d1dfc01528eb828ac5ee3d085bdd09db97
Tayyab Siddiqui <tayyabsiddiqui9892@gmail.com>
2026-05-05 12:28:30 +0530

    Refactor MODEL_PATH for improved readability

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-MODEL_PATH = BASE_DIR / "utils/models/train/weights/best.pt"
+MODEL_PATH = BASE_DIR / "utils" / "models" / "train" / "weights" / "best.pt"
 
 PORT = int(os.getenv("PORT", 1111))
 DEBUG = os.getenv("DEBUG", "False") == "True"
