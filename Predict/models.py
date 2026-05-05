01da2b602fcbb0fcef62b040499cb09653798b91
Tayyab Siddiqui <tayyabsiddiqui9892@gmail.com>
2026-05-05 13:34:08 +0530

    Fix error handling and update model loading function

--- Predict/models.py
+++ Predict/models.py
@@ -3,6 +3,8 @@ from ultralytics import YOLO
 def load_yolo_model(model_path):
     try:
         model = YOLO(model_path)
+        model.fuse = lambda *args, **kwargs: None
         return model
+
     except Exception as e:
-        return {'Error': str(e)}
+        return {"Error": str(e)}
